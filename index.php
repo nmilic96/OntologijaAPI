ce3a162b2688b42e788acece4d2c1ec68200b190
Tomislav Jakopec <tjakopec@gmail.com>
2019-10-17 10:22:25 +0200

    Završena priprema vježbe

--- index.php
+++ index.php
@@ -3,22 +3,6 @@
 require 'vendor/autoload.php';
 use Jakopec\Osoba;
 
-use function GuzzleHttp\json_decode;
-use function GuzzleHttp\json_encode;
-
-Flight::route('/', function(){
-    $poruka=new stdClass();
-    $poruka->poruka="Nepotpuni zahtjev";
-    $poruka->kod=1;
-    $poruka->detalji="https://upute.app.hr/blog/api/v1/greske/7";
-
-    $odgovor=new stdClass();
-    $odgovor->greska=[$poruka];
-
-    Flight::json($odgovor);
-
-});
-
 Flight::route('GET /osobe', function(){
     
     $osoba = new Osoba();
@@ -32,14 +16,28 @@ Flight::route('POST /osobe', function(){
     $osoba = new Osoba();
     
     $poruka=new stdClass();
-    $poruka->poruka=$osoba->dodaj(Flight::request()->data);
-
+    $poruka->tekst=$osoba->dodaj(Flight::request()->data);
+    $poruka->greska=false;
     $odgovor=new stdClass();
-    $odgovor->greska=[$poruka];
+    $odgovor->poruka=$poruka;
     
     Flight::json($odgovor); 
-    
+
     header("HTTP/1.1 201 Created"); //https://gist.github.com/phoenixg/5326222
+
+});
+
+Flight::route('/', function(){
+    $poruka=new stdClass();
+    $poruka->tekst="Nepotpuni zahtjev";
+    $poruka->kod=1;
+    $poruka->greska=true;
+    $poruka->detalji="https://upute.app.hr/blog/api/v1/greske/7";
+
+    $odgovor=new stdClass();
+    $odgovor->poruka=$poruka;
+
+    Flight::json($odgovor);
 
 });
 
